ff2d8c2e72e1ec760dbac1a07e49f6de473f04e1
09112003ika <121323185+09112003ika@users.noreply.github.com>
2024-12-29 00:50:41 +0700

    Add files via upload

--- coffe_shop.sql
+++ coffe_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 26, 2024 at 04:17 PM
+-- Generation Time: Dec 28, 2024 at 06:50 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -29,10 +29,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `menu` (
   `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `category` varchar(50) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `stock` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `category`, `name`, `price`, `description`, `image`, `stock`) VALUES
+(1, 'non-coffee', 'Red Velvet Latte', '25.00', 'Red Velvet Latte combines espresso, steamed milk, and red velvet flavor, topped with cream cheese froth for a sweet, indulgent treat.', NULL, 0),
+(2, 'non-coffee', 'Mojito Mocktail', '23.00', 'Mojito Mocktail is a refreshing non-alcoholic drink made with lime, mint, sugar, and soda water.', 'C:\\xampp\\htdocs\\coffeshop\\mojito.jpeg', 0),
+(3, 'non-coffee', 'Mojito Mocktail', '23.00', 'Mojito Mocktail is a refreshing non-alcoholic drink made with lime, mint, sugar, and soda water.', 'C:\\xampp\\htdocs\\coffeshop\\mojito.jpeg', 0),
+(6, 'non-coffee', 'Matcha Latte', '25000.00', 'Matcha latte is a creamy, slightly bitter drink made from finely ground matcha green tea powder, steamed milk, and a touch of sweetness.', 'C:\\xampp\\htdocs\\coffeshop\\images\\mojito.jpeg', 0);
 
 -- --------------------------------------------------------
 
@@ -58,8 +71,21 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `email_or_phone` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` enum('admin','customer') NOT NULL DEFAULT 'customer'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email_or_phone`, `created_at`, `role`) VALUES
+(1, 'ika', '$2y$10$ujsZ9xn1ccV2ql7wRIQTrOJ7dM1xHzzX.1kPZ0/8MNz0.hDmX4zem', 'ikak@gmail.com', '2024-12-28 08:11:30', 'customer'),
+(2, 'fan', '$2y$10$ZmAqhKDeWq7Xxxt5ivg/k.JhFrr5Laqp.gxdH258ba/JIOir1LXJC', '086748234733', '2024-12-28 08:16:04', 'customer'),
+(4, 'admin12', '$2y$10$rNI2T7DvFY1x7TT.ysIwGuCCbk4N.6m5XXoYLbfitowxRC697nllG', 'ikak123@gmail.com', '2024-12-28 12:09:02', 'admin'),
+(5, 'admin3', '$2y$10$rVyHqTtQ/oYw1F9YnEusd.iZCksfIT1VIN5NYjKlcBhlqsQFK3jim', 'admin3@gmail.com', '2024-12-28 14:34:48', 'admin'),
+(6, 'ikak', '$2y$10$zBBTAkwLXcmJd9VHViIDLuiyp/ZPwSv7lpaXcqv/4FSdEqnTSaR7W', 'ikakk@gmail.com', '2024-12-28 17:48:28', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -83,7 +109,8 @@ ALTER TABLE `orders`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email_or_phone` (`email_or_phone`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -93,7 +120,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -105,7 +132,7 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
